ea43825d8ddc19a9b29c496635302d960e2d575e
invokerkael918 <sam.cao@mail.utoronto.ca>
2019-10-16 00:34:44 -0400

    Triangle optimize

--- Triangle.py
+++ Triangle.py
@@ -6,18 +6,22 @@ class Solution1:
     @return: An integer, minimum path sum
     """
 
-    def minimumTotal(self, triangle):
-        # write your code here
+    def minimumTotal(self, triangle) -> int:
+
+        if not triangle:
+            return 0
+
         n = len(triangle)
         dp = [[0] * (i + 1) for i in range(n)]
 
-        for i in range(n):
-            dp[n - 1][i] = triangle[n - 1][i]
+        # for i in range(n):
+        #     dp[n - 1][i] = triangle[n - 1][i]
+        dp[-1] = triangle[-1]
 
         for i in range(n - 2, -1, -1):
-            # 从n-1到0，每次递减1
             for j in range(i + 1):
-                dp[i][j] = min(dp[i + 1][j + 1], dp[i + 1][j]) + triangle[i][j]
+                dp[i][j] = min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j]
+
         return dp[0][0]
 
 
@@ -109,5 +113,5 @@ if __name__ == '__main__':
         [4, 1, 8, 3]
     ]
 
-    S = Solution4().minimumTotal(a)
+    S = Solution1().minimumTotal(triangle)
     print(S)
