863e8796803841c82ad46e4164ecc5e7bdecbc9c
Sam Cao <sam.cao@scotiabank.com>
2019-10-16 23:13:04 -0400

    added comments for Knight 2

--- Knight Shortest Path II.py
+++ Knight Shortest Path II.py
@@ -6,7 +6,7 @@ direction = [
     (-2, -1),
     (2, -1)
 ]
-
+#取相反数是因为要看从哪个点来
 
 class Solution:
     """
@@ -20,20 +20,31 @@ class Solution:
             return -1
         n, m = len(grid), len(grid[0])
 
-        dp = [[sys.maxsize for j in range(m)] for _ in range(n)]
+        dp = [[100 for j in range(m)] for _ in range(n)]
         dp[0][0] = 0
         for j in range(m):
             for i in range(n):
                 if grid[i][j]:
                     continue
                 for delta_x, delta_y in direction:
+
                     x, y = i + delta_x, j + delta_y
+                    print(x,y)
+                    # 在 x,y 到 区间范围内，之前找了很多 0 0 的前记节点，但是他们不存在棋盘内
+                    # DP是在找从哪个点到当前点的最短路径，逆向求得最短路径
                     if 0 <= x < n and 0 <= y < m:
                         dp[i][j] = min(dp[i][j], dp[x][y] + 1)
+                        self.printBoard(dp)
+                        print("")
         if dp[n - 1][m - 1] == sys.maxsize:
             return -1
 
         return dp[n - 1][m - 1]
+
+    def printBoard(self,dp):
+        for ele in dp:
+            print(ele)
+
 
 
 if __name__ == '__main__':
