d1fc596fef0ff338fd3e83bc53bf0a921ab72789
yuancao <sam.cao918@gmail.com>
2019-10-26 15:43:22 -0400

    format

--- Climbing Stairs.py
+++ Climbing Stairs.py
@@ -6,19 +6,21 @@ class Solution:
     2.如果起始跳二阶的话，剩余的n-2阶就有 f(n-2) 种跳法；
     所以f(n) = f(n-1) + f(n-2)，实际结果即为斐波纳契数。
     """
+
     def climbStairs(self, n):
         # write your code here
         if n == 0:
             return 0
-        if n ==1 :
+        if n == 1:
             return 1
         # if n == 2:
         #     return 2
-        result = [1,2]
+        result = [1, 2]
 
-        for _ in range(n-2):
-            result.append(result[-1]+result[-2])
+        for _ in range(n - 2):
+            result.append(result[-1] + result[-2])
         return result[-1]
+
 
 if __name__ == '__main__':
     S = Solution().climbStairs(7)
