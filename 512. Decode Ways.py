c7791dfd7c584907b634445ebd3c95208f6cfe91
yuancao <sam.cao918@gmail.com>
2019-12-14 15:03:50 -0500

    added comments

--- 512. Decode Ways.py
+++ 512. Decode Ways.py
@@ -25,12 +25,18 @@ class Solution:
         dp[0] = 1
         for i in range(1, len(s)):
             if s[i] in singleDigit:
+                # 先检查当前位置，如果在1位set里，延续dp[i-1]
                 dp[i] += dp[i - 1]
-
+                # 不能continue
             if s[i - 1] + s[i] in twoDigits:
-
+                # 如果
                 if i == 1:
                     dp[i] += 1
+                # 继续检查如果前两位在2位set里，在dp[i-1]基础上继续加上dp[i-2]
                 else:
                     dp[i] += dp[i - 2]
         return dp[-1]
+
+if __name__ == '__main__':
+    S = Solution().numDecodings("123")
+    print(S)
